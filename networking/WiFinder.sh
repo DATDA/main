489b6e3e7435719c2a57ff2bbe9ab1784ccc8494
Bugg <christopherbugg@users.noreply.github.com>
2018-07-21 14:02:00 -0600

    Update WiFinder.sh

--- networking/WiFinder.sh
+++ networking/WiFinder.sh
@@ -4,15 +4,27 @@
 # @Bugg
 # 07/21/18
 
+#Parameters, change these:
+interface="wlan0"
+wifi_name="mywifi"
+
 while true;
 do
 
-num=$(iwlist wlan0 scan | grep -B 2 "mywifi" | grep "Signal" | cut -d = -f 3)
+num=$(iwlist $interface scan | grep -B 2 $wifi_name | grep "Signal" | cut -d = -f 3 | cut -d " " -f 1 | tail -c 3)
 
-len=$(echo $num | cut -d " " -f 1 | tail -c 3)
+if [ ! -z $num ]
+then
 
-bar=$(head -c $len < /dev/zero | tr '\0' '\167')
+bar=$(head -c $num < /dev/zero | tr '\0' '\167')
 
-echo "$num$bar"
+echo $num"dBm $bar"
+
+else
+
+echo "Signal Lost..."
+sleep 1
+
+fi
 
 done
